bee83a590175ce8a384eea4212dc1802fb03f449
阿小庆 <1756260160@qq.com>
2020-06-09 23:24:25 +0800

    退出功能

--- part1实战通讯录管理系统/通讯录管理系统.cpp
+++ part1实战通讯录管理系统/通讯录管理系统.cpp
@@ -4,10 +4,10 @@
  * @Company(School): UCAS
  * @Date: 2020-06-04 01:02:50
  * @LastEditors: HCQ
- * @LastEditTime: 2020-06-04 01:03:03
+ * @LastEditTime: 2020-06-09 23:23:01
  */ 
-#include<iostream>
-using namespace std;
+#include<iostream> // 输入输出
+using namespace std; // 引入标准命名空间
 
 //菜单界面
 void showMenu()
@@ -23,9 +23,40 @@ void showMenu()
 	cout << "***************************" << endl;
 }
 
-int main() {
+int main()
+{
 
-	showMenu();
+	int select = 0;
+
+	while (true)
+	{
+		showMenu();
+
+		cin >> select;
+
+		switch (select)
+		{
+		case 1: //添加联系人
+			break;
+		case 2: //显示联系人
+			break;
+		case 3: //删除联系人
+			break;
+		case 4: //查找联系人
+			break;
+		case 5: //修改联系人
+			break;
+		case 6: //清空联系人
+			break;
+		case 0: //退出通讯录
+			cout << "欢迎下次使用" << endl;
+			system("pause");
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
 
 	system("pause");
 
