3c2625214a4e5e81408dc826bb2e3ec75e746afb
HuangCongQing <1756260160@qq.com>
2021-03-03 18:50:03 +0800

    update

--- practice/01参数.cpp
+++ practice/01参数.cpp
@@ -5,10 +5,12 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2021-03-03 18:36:31
- * @LastEditTime: 2021-03-03 18:42:25
+ * @LastEditTime: 2021-03-03 18:49:55
  * @FilePath: /c-plus-plus/practice/01参数.cpp
  */
 #include<iostream>  
+#include <boost/filesystem.hpp>
+#include <boost/foreach.hpp>
 using namespace std;
 
 
@@ -27,5 +29,14 @@ int main(int argc, char *argv[]) {
   cout<<bag_path<<endl;
   std::string source_folder = argv[2];  // ../src/pcd_all
   cout<<source_folder<<endl;
+  // // 判断文件类型
+  // bag_path = "asgd.bag";
+  // boost::filesystem::path f(bag_path);
+  // std::string extensionname = f.extension().string();
+  // if (extensionname != ".bag") {   // 判断是不是bag文件
+  //   std::cerr << "Not a bag  !" << std::endl;
+  //   }else{
+  //       std::cerr << "is  a bag  !" << std::endl;
+  //   }
 
 }
