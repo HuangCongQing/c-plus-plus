8e3ee70f2318d74046368e05f471cad5caf5d8d4
HuangCongQing <1756260160@qq.com>
2021-04-14 15:45:04 +0800

    update

--- practice/02c++读取txt文件按空格分割字符串.cpp
+++ practice/02c++读取txt文件按空格分割字符串.cpp
@@ -4,7 +4,7 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2021-03-03 18:06:38
- * @LastEditTime: 2021-03-04 14:17:50
+ * @LastEditTime: 2021-04-14 15:44:49
  * @FilePath: /c-plus-plus/practice/02c++读取txt文件按空格分割字符串.cpp
  */
 
@@ -23,13 +23,15 @@ int main()
 	char line[1024] = { 0 };
 	ifstream infile;
 	infile.open("/home/hcq/data/pcd/RS-LiDAR-16-pcd/process/1134off-ground points.txt"); // txt文件
+    int lineCnt=0;
     while(	infile.getline(line, sizeof(line))){ // 遍历每行
         stringstream word(line);  //采用字符流格式将读取的str进行空格分隔，并放入str word中
         word >> x >> y >> z;
-    
+    	lineCnt++;
         cout << x << " " << y << " " << z << endl;   // 小数点4位  -12.8349 -2.35511 -1.0137
         // cout << fixed << setprecision(9) << x << " " << y << " " << z << endl;   // 小数点9位 -2.295508150 -0.421606420 2.291652680
     }
+    cout << "行数"<<lineCnt+1<<endl;
  
 
    return 0;
--- practice/tempCodeRunnerFile.cpp
+++ practice/tempCodeRunnerFile.cpp
@@ -1,7 +1,1 @@
-auto start = system_clock::now();
-// // do something...
-// auto end   = system_clock::now();
-// auto duration = duration_cast<microseconds>(end - start);
-// cout <<  "花费了" 
-//      << double(duration.count()) * microseconds::period::num / microseconds::period::den 
-//      << "秒" << endl;
+cout<<"行数"<<lineCnt+1<<endl;
