52ea1712914c30f1f5baa286897c3de90bbf73e2
HuangCongQing <1756260160@qq.com>
2021-04-15 16:20:13 +0800

    update

--- data-process/遍历文件路径名/path.cpp
+++ data-process/遍历文件路径名/path.cpp
@@ -7,7 +7,7 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2021-04-15 15:57:19
- * @LastEditTime: 2021-04-15 16:16:59
+ * @LastEditTime: 2021-04-15 16:20:07
  * @FilePath: /c-plus-plus/data-process/遍历文件路径名/path.cpp
  */
 // 遍历文件
@@ -25,12 +25,14 @@ int main()
     for (auto &it : boost::filesystem::directory_iterator(pcd_all_path))
     {
         std::string strPath = it.path().string(); //遍历出来的文件名称
-        std::string filename = it.path().filename().string(); //遍历出来的文件名称
-        std::string extension = it.path().extension().string(); //遍历出来的文件名称
+        std::string filename = it.path().filename().string(); //文件名称
+        std::string stem = it.path().stem().string(); //不带后缀的文件名
+        std::string extension = it.path().extension().string(); //后缀名
 
-        std::cout << "strPath:" << strPath << std::endl;
-        std::cout <<  " filename:" << filename << std::endl;  // filename:1600050937.416284.pcd
-        std::cout <<  " 后缀名:" << extension << std::endl;  //  后缀名:.pcd
+        std::cout << "完整路径:" << strPath << std::endl;
+        std::cout <<  "文件名:" << filename << std::endl;  // filename:1600050937.416284.pcd
+        std::cout <<  "不带后缀的文件名:" << stem << std::endl;  //不带后缀的文件名:1600050937.416284
+        std::cout <<  "后缀名:" << extension << std::endl;  //  后缀名:.pcd
     }
     return 0;
 }
