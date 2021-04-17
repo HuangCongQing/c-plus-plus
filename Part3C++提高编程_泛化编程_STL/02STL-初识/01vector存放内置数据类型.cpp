1166ca96724a9d86398a867a56beb18d7bd97191
HuangCongQing <1756260160@qq.com>
2021-04-17 12:11:36 +0800

    add: 模板

---
+++ Part3C++提高编程_泛化编程_STL/02STL-初识/01vector存放内置数据类型.cpp
@@ -0,0 +1,10 @@
+/*
+ * @Description: vector存放内置数据类型 参考video:https://www.bilibili.com/video/BV1Hb411Y7E5?p=186&spm_id_from=pageDriver
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-04-17 12:02:45
+ * @LastEditTime: 2021-04-17 12:03:11
+ * @FilePath: /c-plus-plus/Part3C++提高编程_泛化编程_STL/02STL-初识/01vector存放内置数据类型.cpp
+ */
+
--- template.cpp
+++ template.cpp
@@ -4,7 +4,7 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2021-04-17 11:24:57
- * @LastEditTime: 2021-04-17 11:25:49
+ * @LastEditTime: 2021-04-17 11:27:55
  * @FilePath: /c-plus-plus/template.cpp
  */
 #include<iostream>  
@@ -13,6 +13,6 @@ using namespace std;
 
 
 int main(int argc, char *argv[]) {
-    printf("这是模板");
+    cout << "这是模板" << endl;
     return 0;
 }
