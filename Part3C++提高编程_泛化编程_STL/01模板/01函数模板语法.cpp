d9b1112f8a2a16bba5d67f5d19c6e58f8ece213d
HuangCongQing <1756260160@qq.com>
2021-04-17 11:25:56 +0800

    update

---
+++ Part3C++提高编程_泛化编程_STL/01模板/01函数模板语法.cpp
@@ -0,0 +1,9 @@
+/*
+ * @Description: 函数模板
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-04-17 11:23:56
+ * @LastEditTime: 2021-04-17 11:24:04
+ * @FilePath: /c-plus-plus/Part3C++提高编程_泛化编程_STL/01模板/01函数模板语法.cpp
+ */
---
+++ template.cpp
@@ -0,0 +1,18 @@
+/*
+ * @Description: 
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-04-17 11:24:57
+ * @LastEditTime: 2021-04-17 11:25:49
+ * @FilePath: /c-plus-plus/template.cpp
+ */
+#include<iostream>  
+using namespace std;
+
+
+
+int main(int argc, char *argv[]) {
+    printf("这是模板");
+    return 0;
+}
