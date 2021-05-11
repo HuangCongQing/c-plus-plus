c011b45e5af5978ac06bdbd90525dc827242d940
HuangCongQing <1756260160@qq.com>
2021-05-11 15:36:58 +0800

    update

--- Part3C++提高编程_泛化编程_STL/03STL-常用容器/3.1string/05string容器_字符串比较.cpp
+++ Part3C++提高编程_泛化编程_STL/03STL-常用容器/3.1string/05string容器_字符串比较.cpp
@@ -1,33 +1,42 @@
-//#include<iostream>
-//using namespace std;
-//
-////字符串比较
-//
-//void test01()
-//{
-//	string str1 = "hello";
-//	string str2 = "xello";
-//
-//	if (str1.compare(str2) == 0)
-//	{
-//		cout << "str1 等于 str2" << endl;
-//	}
-//	else if (str1.compare(str2) > 0)
-//	{
-//		cout << "str1 大于 str2" << endl;
-//	}
-//	else
-//	{
-//		cout << "str1 小于 str2" << endl;
-//	}
-//
-//}
-//
-//int main() {
-//
-//	test01();
-//
-//	system("pause");
-//
-//	return 0;
-//}
+/*
+ * @Description: 
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-05-11 15:02:08
+ * @LastEditTime: 2021-05-11 15:33:16
+ * @FilePath: /c-plus-plus/Part3C++提高编程_泛化编程_STL/03STL-常用容器/3.1string/05string容器_字符串比较.cpp
+ */
+#include<iostream>
+using namespace std;
+
+//字符串比较
+
+void test01()
+{
+	string str1 = "hello";
+	string str2 = "xello";
+
+	if (str1.compare(str2) == 0)
+	{
+		cout << "str1 等于 str2" << endl;
+	}
+	else if (str1.compare(str2) > 0)
+	{
+		cout << "str1 大于 str2" << endl;
+	}
+	else
+	{
+		cout << "str1 小于 str2" << endl;
+	}
+
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
--- Part3C++提高编程_泛化编程_STL/03STL-常用容器/3.1string/06string容器_字符存取.cpp
+++ Part3C++提高编程_泛化编程_STL/03STL-常用容器/3.1string/06string容器_字符存取.cpp
@@ -1,42 +1,51 @@
-//#include<iostream>
-//using namespace std;
-//#include <string>
-////string 字符存取
-//void test01()
-//{
-//	string str = "hello";
-//
-//	//cout << "str = " << str << endl;
-//
-//	//1、通过 []访问单个字符
-//	for (int i = 0; i < str.size(); i++)
-//	{
-//		cout << str[i] << " ";
-//	}
-//	cout << endl;
-//
-//	//2、通过at方式访问单个字符
-//	for (int i = 0; i < str.size(); i++)
-//	{
-//		cout << str.at(i) << " ";
-//	}
-//	cout << endl;
-//
-//	//修改单个字符
-//	str[0] = 'x';
-//	// xello
-//	cout << "str = " << str << endl;
-//
-//	str.at(1) = 'x';
-//	// xxllo
-//	cout << "str = " << str << endl;
-//}
-//
-//int main() {
-//
-//	test01();
-//	
-//	system("pause");
-//
-//	return 0;
-//}
+/*
+ * @Description: 
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-05-11 15:02:08
+ * @LastEditTime: 2021-05-11 15:34:59
+ * @FilePath: /c-plus-plus/Part3C++提高编程_泛化编程_STL/03STL-常用容器/3.1string/06string容器_字符存取.cpp
+ */
+#include<iostream>
+using namespace std;
+#include <string>
+//string 字符存取
+void test01()
+{
+	string str = "hello";
+
+	//cout << "str = " << str << endl;
+
+	//1、通过 []访问单个字符
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << str[i] << " ";
+	}
+	cout << endl;
+
+	//2、通过at方式访问单个字符
+	for (int i = 0; i < str.size(); i++)
+	{
+		cout << str.at(i) << " ";
+	}
+	cout << endl;
+
+	//修改单个字符
+	str[0] = 'x';
+	// xello
+	cout << "str = " << str << endl;
+
+	str.at(1) = 'x';
+	// xxllo
+	cout << "str = " << str << endl;
+}
+
+int main() {
+
+	test01();
+	
+	system("pause");
+
+	return 0;
+}
