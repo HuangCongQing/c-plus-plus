134b3d30484eac935a14defb30ee726c436b3820
HuangCongQing <1756260160@qq.com>
2021-05-11 10:39:39 +0800

    构造赋值

--- Part3C++提高编程_泛化编程_STL/03STL-常用容器/3.9map容器/01 map容器_构造和赋值.cpp
+++ Part3C++提高编程_泛化编程_STL/03STL-常用容器/3.9map容器/01 map容器_构造和赋值.cpp
@@ -1,13 +1,19 @@
 #include<iostream>
 using namespace std;
 #include <map>
+/* 
+//创建map容器
+map<int, int> m;
+
+
+ */
 
 //map容器 构造和赋值
 void printMap(map<int, int>&m)
 {
 	for (map<int, int>::iterator it = m.begin(); it != m.end(); it++)
 	{
-		cout << "key = " << (*it).first << " value = " << it->second << endl;
+		cout << "key = " << (*it).first << " value = " << it->second << endl;  // 输出first second
 	}
 	cout << endl;
 }
@@ -17,7 +23,7 @@ void test01()
 	//创建map容器
 	map<int, int> m;
 
-	m.insert(pair<int, int>(1, 10));
+	m.insert(pair<int, int>(1, 10));  // 插入pair
 	m.insert(pair<int, int>(3, 30));
 	m.insert(pair<int, int>(2, 20));
 	m.insert(pair<int, int>(4, 40));
