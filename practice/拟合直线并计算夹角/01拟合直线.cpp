0c2b03397e2d90a184798d4df75a5105c897be90
HuangCongQing <1756260160@qq.com>
2021-05-19 18:14:02 +0800

    init

---
+++ practice/拟合直线并计算夹角/01拟合直线.cpp
@@ -0,0 +1,11 @@
+/*
+ * @Description: 拟合直线并计算夹角
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-05-19 18:11:47
+ * @LastEditTime: 2021-05-19 18:11:47
+ * @FilePath: /c-plus-plus/practice/拟合直线并计算夹角/拟合直线并计算夹角.cpp
+ */
+
+
---
+++ practice/拟合直线并计算夹角/02计算两夹角.cpp
@@ -0,0 +1,11 @@
+/*
+ * @Description: 计算两直线夹角
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-05-19 18:13:43
+ * @LastEditTime: 2021-05-19 18:13:53
+ * @FilePath: /c-plus-plus/practice/拟合直线并计算夹角/02计算两夹角.cpp
+ */
+
+
