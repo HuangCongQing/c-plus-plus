10c494719bce123864ffd7c9223368c5cd6cd693
hcq <1756260160@qq.com>
2022-06-20 17:02:12 +0800

    add: 指针和函数

---
+++ part1基础/指针/07指针和函数.cpp
@@ -0,0 +1,45 @@
+/*
+ * @Description: 
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2021-04-17 11:24:57
+ * @LastEditTime: 2022-06-20 17:00:53
+ * @FilePath: /c-plus-plus/part1基础/指针/07指针和函数.cpp
+ */
+#include<iostream>  
+using namespace std;
+
+
+
+//值传递
+void swap1(int a ,int b)
+{
+	int temp = a;
+	a = b; 
+	b = temp;
+}
+//地址传递
+void swap2(int * p1, int *p2)
+{
+	int temp = *p1;
+	*p1 = *p2;
+	*p2 = temp;
+}
+
+int main() {
+
+	int a = 10;
+	int b = 20;
+	swap1(a, b); // 值传递不会改变实参
+
+	swap2(&a, &b); //地址传递会改变实参
+
+	cout << "a = " << a << endl;
+
+	cout << "b = " << b << endl;
+
+	system("pause");
+
+	return 0;
+}
