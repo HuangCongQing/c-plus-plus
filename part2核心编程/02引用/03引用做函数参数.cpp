a554b81613c140d12ed9db970e8681ac67922fd8
hcq <1756260160@qq.com>
2022-06-21 12:50:55 +0800

    note: 引用

--- part2核心编程/02引用/03引用做函数参数.cpp
+++ part2核心编程/02引用/03引用做函数参数.cpp
@@ -1,10 +1,10 @@
 /*
- * @Description: 
+ * @Description: 引用传递 总结：通过引用参数产生的效果同按地址传递是一样的。引用的语法更清楚简单
  * @Author: HCQ
  * @Company(School): UCAS
  * @Date: 2020-09-16 17:31:48
- * @LastEditors: HCQ
- * @LastEditTime: 2020-09-16 17:32:46
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-06-21 12:22:46
  */
 #include <iostream>
 using namespace std;
@@ -23,7 +23,7 @@ void mySwap02(int *a, int *b)
     *a = *b;
     *b = temp;
 }
-//3. 引用传递
+//3. 引用传递===============================================================
 void mySwap03(int &a, int &b)
 {
     int temp = a;
@@ -38,8 +38,13 @@ int main()
     cout << "a:" << a << " b:" << b << endl;
     mySwap02(&a, &b);
     cout << "a:" << a << " b:" << b << endl;
-    mySwap03(a, b);
+    mySwap03(a, b); // 总结：通过引用参数产生的效果同按地址传递是一样的。引用的语法更清楚简单
     cout << "a:" << a << " b:" << b << endl;
     system("pause");
     return 0;
 }
+/* 
+a:10 b:20
+a:20 b:10
+a:10 b:20
+ */
--- part2核心编程/02引用/04引用做函数返回值.cpp
+++ part2核心编程/02引用/04引用做函数返回值.cpp
@@ -1,10 +1,10 @@
 /*
- * @Description: 
+ * @Description: 如果函数的返回值是引用，这个函数调用可以作为左值=
  * @Author: HCQ
  * @Company(School): UCAS
  * @Date: 2020-09-16 17:31:48
- * @LastEditors: HCQ
- * @LastEditTime: 2020-09-16 18:03:03
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-06-21 12:50:26
  */
 #include <iostream>
 using namespace std;
@@ -12,26 +12,26 @@ using namespace std;
 //返回局部变量引用
 int &test01()
 {
-    int a = 10; //局部变量
+    int a = 10; //局部变量 结束内存已释放
     return a;
 }
 //返回静态变量引用
 int &test02()
 {
-    static int a = 20; // 静态变量存放在全局区，最后、程序结束后才释放掉
+    static int a = 20; // 静态变量存放在全局区，最后、程序结束后才释放掉========================================================
     return a;
 }
 int main()
 {
     //不能返回局部变量的引用
-    int &ref = test01();
+    int & ref = test01();
     cout << "ref = " << ref << endl;// 第一次结果正确，是因为编译器做了保留
     cout << "ref = " << ref << endl; // 第二次结果错误，是因为a的内存已经释放
     //如果函数做左值，那么必须返回引用
-    int &ref2 = test02();
+    int & ref2 = test02();
     cout << "ref2 = " << ref2 << endl;
     cout << "ref2 = " << ref2 << endl;
-    test02() = 1000; //  如果函数的返回值是引用，这个函数调用可以作为左值
+    test02() = 1000; //  如果函数的返回值是引用，这个函数调用可以作为左值=====================================================
     cout << "ref2 = " << ref2 << endl;
     cout << "ref2 = " << ref2 << endl;
     system("pause");
