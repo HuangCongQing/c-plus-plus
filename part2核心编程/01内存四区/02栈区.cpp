ffe291e6a03925192108f695980dbe60ec60614d
hcq <1756260160@qq.com>
2022-06-21 11:05:42 +0800

    note: 栈区

--- part2核心编程/01内存四区/02栈区.cpp
+++ part2核心编程/01内存四区/02栈区.cpp
@@ -1,24 +1,26 @@
 /*
- * @Description: 
+ * @Description: 注意事项：不要返回局部变量的地址，栈区开辟的数据由编译器自动释放
  * @Author: HCQ
  * @Company(School): UCAS
  * @Date: 2020-06-10 10:22:47
- * @LastEditors: HCQ
- * @LastEditTime: 2020-06-10 11:19:37
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2022-06-21 10:59:49
  */ 
 #include <iostream>
 using namespace std;
 
-int *func()
+// 注意事项：不要返回局部变量的地址，栈区开辟的数据由编译器自动释放
+int* func()
 {
-    int a = 10;
-    return &a;
+    int a = 10; // 局部变量存放在栈区（栈区的数据在函数执行完释放）
+    return &a; // 返回地址
 }
 int main()
-{
-    int *p = func();
-    cout << *p << endl;
-    cout << *p << endl;
+{   
+    // 接收func的返回值
+    int * p = func();
+    cout << *p << endl; // 第一次可以打印10，因为编译器作了保留
+    cout << *p << endl; // 第二次不再保留
     system("pause");
     return 0;
 }
