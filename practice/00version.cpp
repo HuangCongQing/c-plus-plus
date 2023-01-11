bb30f06ce314de0fc5e09df8ec140da4905af5f8
hcq <1756260160@qq.com>
2023-01-11 22:31:45 +0800

    feat: 查看version版本

---
+++ practice/00version.cpp
@@ -0,0 +1,17 @@
+/*
+ * @Description: 查看C++ 版本 https://www.yuque.com/huangzhongqing/lang/utyxcg
+ * @Author: HCQ
+ * @Company(School): UCAS
+ * @Email: 1756260160@qq.com
+ * @Date: 2023-01-11 22:03:02
+ * @LastEditTime: 2023-01-11 22:03:09
+ * @FilePath: /c-plus-plus/practice/00version.cpp
+ */
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!\n";
+  std::cout << __cplusplus << std::endl; // 201402
+  return 0;
+}
