2623901190d8fbc7868418cf4f4af7b257acece4
chongqinghuang <chongqinghuang@autox.ai>
2023-04-28 14:53:53 +0800

    void指针遍历以为数组

--- part1基础/指针/数组和指针/01demo.cpp
+++ part1基础/指针/数组和指针/01demo.cpp
@@ -4,7 +4,7 @@
  * @Company(School): UCAS
  * @Email: 1756260160@qq.com
  * @Date: 2023-04-27 18:12:32
- * @LastEditTime: 2023-04-27 19:23:32
+ * @LastEditTime: 2023-04-28 14:53:26
  * @FilePath: /c-plus-plus/part1基础/指针/数组和指针/01demo.cpp
  */
 #include <iostream>
@@ -29,3 +29,22 @@ int main()
 *(p + 4) : 50
 
 */
+
+
+// for void* https://blog.csdn.net/weixin_55420595/article/details/121087721
+
+// #include<stdio.h>
+// int main()
+// {	
+// 	int arr[] = {1,2,3,4,5,6};
+// 	void * p;
+// 	int  i ,n;
+// 	p = arr;
+// 	n = sizeof(arr)/sizeof(int);
+// 	for(i = 0;i < n;i++)
+// 	{
+// 		printf("%d",*((int *)p + i));
+// 	 }
+// 	puts("");
+// 	return 0;
+// }
